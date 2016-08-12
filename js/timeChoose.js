3b925ada9a35c21ddbfc12c7e5ca32813abd0077
a982503660 <982503660@qq.com>
2016-08-12 15:53:53 +0800

    基于原生js的时间段选择控件

--- js/timeChoose.js
+++ js/timeChoose.js
@@ -1,5 +1,5 @@
 
-function daynumber(month) { //判断月份的天数函数
+function daynumber(month) {                                 //判断月份的天数函数
     var date = new Date();
     var year = date.getFullYear();
     var month;
@@ -15,32 +15,32 @@ function daynumber(month) { //判断月份的天数函数
     } else {
         days = 31;
     }
-    return days; //返回月份天数
+    return days;                                            //返回月份天数
 }
 
-function datequick() { 		//快捷日期选择函数
+function datequick() { 		                                //快捷日期选择函数
     var sel = document.getElementById("datequick").value; 	//获得下拉框的值
-    var date = new Date(); 					//创建日期实例
-    var day = date.getDate(); 				//获取今天的日期
-    var month = (date.getMonth() + 1); 		//获取当前月份需加1 JS月份是从0计算
-    var year = date.getFullYear(); 			//获取当前年份
-    switch(sel) { 							//根据获得的下拉框值执行
-        case "1": //昨天
+    var date = new Date(); 					                //创建日期实例
+    var day = date.getDate(); 				                //获取今天的日期
+    var month = (date.getMonth() + 1); 		                //获取当前月份需加1 JS月份是从0计算
+    var year = date.getFullYear(); 			                //获取当前年份
+    switch(sel) { 							                //根据获得的下拉框值执行
+        case "1":                                           //昨天
             day1 = day - 1;
             if(day1 == 0) {
-                month -= 1; 		//月份减一
+                month -= 1; 		                        //月份减一
                 days = daynumber(month); 					//调用daymunber函数并传值
                 dateset(year, month, days, month, day1); 	//调用dataset函数并传值
                 break;
             }
             dateset(year, month, day1, month, day1);
             break;
-        case "2": //今天
+        case "2":                                           //今天
             dateset(year, month, day, month, day);
             break;
-        case "3": //上周
-            var day1 = day - 7; 		//今天日期往前算7天
-            var day2 = day - 14; 		//今天日期往前算14天
+        case "3":                                           //上周
+            var day1 = day - 7; 		                    //今天日期往前算7天
+            var day2 = day - 14; 		                    //今天日期往前算14天
             if(day1 <= 0) {
                 month -= 1;
                 days = daynumber(month);
@@ -58,11 +58,11 @@ function datequick() { 		//快捷日期选择函数
             dateset(year, month, day2, month, day1);
             break;
         case "4": //本周  以今天日期往前算为7天
-            var day1 = day - 7; 			//有可能会变成负数
-            if(day1 <= 0) { 				//条件需为小于或等于0
-                month1 = month - 1; 		//月份减一
-                days = daynumber(month); 	//调用daymunber函数返回此月份有几天
-                days += day1; 				// days天数加上day1  day1是负数
+            var day1 = day - 7; 			                //有可能会变成负数
+            if(day1 <= 0) { 				                //条件需为小于或等于0
+                month1 = month - 1; 		                //月份减一
+                days = daynumber(month); 	                //调用daymunber函数返回此月份有几天
+                days += day1; 				                // days天数加上day1  day1是负数
                 dateset(year, month1, days, month, day);
             } else {
                 dateset(year, month, day1, month, day);
